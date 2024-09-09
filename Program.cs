8a5310854d6cb5d6a390f4a0d5453520a6615643
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-09-09 13:05:22 +0200

    lagt till kontroll av inmatning av datum samt felmeddelande om det är fel. skapat variabler för dag, månad, år, århundrade och år i det århundradet. Justerat månaden enligt Zellers algoritm samt fått ut en siffra för veckodag

--- Program.cs
+++ Program.cs
@@ -1,5 +1,47 @@
 ﻿// Code by Anne-Lii Hansen anha2324@student.miun.se
-Console.WriteLine("Skriv ett datum (YYYY-MM-DD):");//uppmaning att skriva ett datum
- string input = Console.ReadLine(); //inmatade datumet
 
- Console.WriteLine("Detta är det inmatade datumet: " + input);
+using System;// inkluderar funktioner som Console och DateTime
+using System.Globalization;//inkludera hantering av datum, tid och nummerformat
+
+class Program //definierar huvudklassen med namnet Program
+{
+
+    static void Main(string[] args) //startpunkt main, startar med denna kod
+    {
+        Console.WriteLine("Skriv ett datum (YYYY-MM-DD):");//uppmaning att skriva ett datum
+        string input = Console.ReadLine(); //inmatade datumet
+
+        Console.WriteLine("Detta är det inmatade datumet: " + input);
+
+       //parsa input till ett giltigt datum
+       if (!DateTime.TryParseExact(input, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date))
+       {
+            Console.WriteLine("Felaktigt datum eller format YYYY-MM-DD");//felmeddelande vid fel format eller felaktigt datum
+            return;
+       }
+
+       int year = date.Year; // inmatat år
+       int month = date.Month; // inmatad månad
+       int day= date.Day; // inmatad dag
+
+        //justera enligt Zellers algoritm. Om januari(1) och februari(2) lägg till 12 på månadsnumret och ta bort ett år
+       if (month < 3)
+       {
+        month += 12;
+        year -= 1;
+       }
+
+       //räkna ut århundrade och året inom århundradet
+       int century = year / 100; //räknar ut århundrade
+       int yearOfCentury = year % 100; //modulusoperatorn för att få fram året inom seklet
+
+       //uträkning av veckodag med Zellers algoritm
+       //weekday = (d + ((13*(m+1))/5) + y + (y/4) + (c/4) + 5*c ) % 7;
+       int weekday = (day + (13 * (month + 1)) / 5 + yearOfCentury + (yearOfCentury / 4) + (century / 4) + (5 * century)) % 7; // ger resultat i tal 0 till 6 där 0 är lördag.
+
+        Console.WriteLine(weekday);
+
+
+    }
+}
+
